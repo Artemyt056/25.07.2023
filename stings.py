212579653cd006fa5814a0dcecb0f6f4c5f26972
Artemyt <artemyt056@gmail.com>
2023-07-25 20:02:40 +0300

    intro strings

---
+++ stings.py
@@ -0,0 +1,18 @@
+my_cat = 'my cat'
+
+my_greeting = "i'm David"
+my_greeting2 = "i\'m \nDavid and \tI\'m a man!!!"
+print(my_greeting2)
+
+chunk = """Ми с другом пішли на річку та побачили великого  вужа"""
+
+myltiplicated = my_cat * 15
+print(myltiplicated)
+print('*' * 150)
+
+summa = my_greeting + my_cat
+print(summa)
+print(chunk)
+
+skier_unicode = '\U000026F7'
+print(skier_unicode)
